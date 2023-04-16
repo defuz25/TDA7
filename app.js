d44dda1f15b5bbe0f96d4db2eef2a1260bc24fc5
defuz25 <96631086+defuz25@users.noreply.github.com>
2023-04-16 19:38:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const path = require('path');
 
 let html = fs.readFileSync('public/c.html', 'utf8');
-app.get("/", function(req, res) {
-  res.send('ok3');
-});
+// app.get("/", function(req, res) {
+//   res.send('ok3');
+// });
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, '/c.html'));
 });
