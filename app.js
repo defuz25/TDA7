930371879a5871105391688980766aebe6a82f3a
defuz25 <96631086+defuz25@users.noreply.github.com>
2023-04-16 18:55:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const fs = require('fs')
 
 let html = fs.readFileSync('public/c.html', 'utf8');
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type('html').send('aaa'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
