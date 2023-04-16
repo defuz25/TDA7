9727b8fae615e7864ce10b42d500921176c5e845
defuz25 <96631086+defuz25@users.noreply.github.com>
2023-04-16 18:34:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-func();
+
+let html = fs.readFile('public/c.html', 'utf8');
 
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-async function func() {
-	let html = await fs.promises.readFile('public/c.html', 'utf8');
-  console.log(html)
-}
