5684212b5413e6aea5ac7240f02efb691737a6bf
defuz25 <96631086+defuz25@users.noreply.github.com>
2023-04-16 18:39:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const fs = require('fs')
 
 let html = fs.readFile('public/c.html', 'utf8');
 
