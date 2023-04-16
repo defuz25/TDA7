8c897ef5b02d3bd2db6f79a587a8e398759f34a5
defuz25 <96631086+defuz25@users.noreply.github.com>
2023-04-16 20:58:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
 
-let html = fs.readFileSync('public/c.html', 'utf8');
+app.use(express.static(__dirname + '/public/'));
 // app.get("/", function(req, res) {
 //   res.send('ok3');
 // });
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/public/c.html');
+  res.sendFile('c.html');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
