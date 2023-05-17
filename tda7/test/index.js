bb2b32abb3a5f1d19d02655628af5ed172696498
defuz25 <96631086+defuz25@users.noreply.github.com>
2023-05-17 16:15:15 +0300

    Update index.js

--- tda7/test/index.js
+++ tda7/test/index.js
@@ -114,8 +114,8 @@ function cutscenes(){
     // $('#dwind').fadeOut(500);
     $('#idkwtf').css({background: 'radial-gradient( #00000000, 65%, black)'})
     $('#idkwtf').animate({opacity: '1'}, 700);
-    $('#dwind').animate({top: '75%'},1000,'swing',()=>print(cats[i],()=>$('#cont').fadeIn(100)));
-    $('#dwind').click(()=>{
+    $('#dwind').animate({top: '75%'},1000,'swing',()=>print(cats[i],()=>$('#cont').css({visibility: 'visible'});));
+    $('#cont').click(()=>{
         $('#cont').css('visibility', 'hidden');
         if(typeof txt!='number'){
             print(cats[++i],()=>{
