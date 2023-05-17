fc7079bd095c349ea5c7d11060136259e4f3f8bf
defuz25 <96631086+defuz25@users.noreply.github.com>
2023-05-17 13:04:36 +0300

    Update index.js

--- tda7/test/index.js
+++ tda7/test/index.js
@@ -101,9 +101,9 @@ function cutscenes(){
     $('.dansw').fadeOut(100);
     $('#dwind').animate({top: '150%'},1000,'swing',()=>$('#dwind').css({visibility: 'hidden'}));
     // $('#dwind').fadeOut(500);
-    $('#idkwtf').css({background: 'radial-gradient( #00000000, 65%, #00000071)'})
+    // $('#idkwtf').css({background: 'radial-gradient( #00000000, 65%, #00000071)'})
     $('#idkwtf').fadeIn(400);
-    $('#dwind').animate({top: '70%'},1000);
+    $('#dwind').animate({top: '75%'},1000);
 }
 
 $('.dansw').click(function() {
